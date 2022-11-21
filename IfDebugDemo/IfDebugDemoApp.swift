5c64b14b54380f01720dab304c84c4ad7b419744
Davide Troise <davide@troise.net>
2022-11-21 22:19:40 +0100

    Initial Commit

---
+++ IfDebugDemo/IfDebugDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  IfDebugDemoApp.swift
+//  IfDebugDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct IfDebugDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
