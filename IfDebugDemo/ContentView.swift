560c188ab9d2810fd7be60509f7f4b1a0eb2aecf
Davide Troise <davide@troise.net>
2022-11-22 14:48:32 +0100

    Complete code

--- IfDebugDemo/ContentView.swift
+++ IfDebugDemo/ContentView.swift
@@ -8,15 +8,34 @@
 
 import SwiftUI
 
+let realData = ["Dog", "Cat", "Fish"]
+let fakeData = ["Animal 0", "Animal 1", "Animal 2"]
+
+#if DEBUG
+let data = fakeData
+let title = "DEBUG VERSION"
+#else
+let data = realData
+let title = "List App"
+#endif
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List(data, id: \.self) { animal in
+                Text(animal)
+            }
+            .navigationTitle(Text(title))
+            #if DEBUG
+            .toolbar {
+                ToolbarItem {
+                    Button("Reload") {
+                        print("reloading...")
+                    }
+                }
+            }
+            #endif
         }
-        .padding()
     }
 }
 
